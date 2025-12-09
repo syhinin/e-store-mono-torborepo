5772c430f963b713fb53f90c59932af6ccdefdcf
Oleksandr Sukhinin <syhinin@gmail.com>
2025-12-09 09:08:07 +0100

    add product-service application

---
+++ apps/product-service/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3002", "http://localhost:3003"],
+    credentials: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Product service is running on port ${PORT}`);
+});
